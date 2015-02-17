69e6218bb534ef7fb6789f5babb857cd459214e7
htreutle <htreutle@ipb-halle.de>
2015-02-17 14:49:34 +0100

    added utility for the creation of a pwm from an alignment

---
+++ R/utilities.R
@@ -0,0 +1,27 @@
+
+##' Creates a matrix-representation of a PWM from a set of sequences
+##'
+##' @title Create PWM from alignment
+##' @param alignment a vector or list of sequences each with equal length
+##' @param alphabet of type Alphabet
+##' @param pseudoCount the number of pseudo-observations for each character in the alphabet
+##' @return PWM as matrix
+##' @export
+##' @author Hendrik Treutler
+getPwmFromAlignment = function(alignment, alphabet, pseudoCount) {
+  alphabetSize <- alphabet$size
+  alignmentLength <- nchar(alignment[[1]])
+  numberOfSequences <- length(alignment)
+  
+  pwm <- matrix(nrow = alphabetSize, ncol = alignmentLength)
+  colnames(pwm) <- 1:alignmentLength
+  rownames(pwm) <- alphabet$chars
+  
+  for(charIdx in 1:alphabetSize){
+    for(posIdx in 1:alignmentLength){
+      pwm[charIdx, posIdx] <- (length(which(substr(alignment, posIdx, posIdx) == alphabet$chars[[charIdx]])) + pseudoCount) / (numberOfSequences + pseudoCount * alphabetSize)
+    }
+  }
+  
+  return(pwm);
+}
